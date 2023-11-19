978ba55f3702065d220c97e8bff9e3c1d0531a63
hakan <tasbeyhakan@gmail.com>
2023-11-19 19:38:01 +0300

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const searchInput = document.querySelector("#search");
 const searchBar = document.querySelector("#search-bar");
 const countries = document.querySelector(".countries");
+
+
+const getApi = async()=>{
+    try{
+        const res = await fetch("https://restcountries.com/v3.1/all")
+        if(res.ok){
+            console.log(res); //? Dogru calisiyor
+            const data = await res.json() //? Json ladik
+            console.log(data);
+            // showData(data)
+            selectBarOptions(data)
+        }else{
+            throw new Error(`Hata olustu: ${res.ststus}`)
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+getApi()
+
+
+
+const countriesArr = []
+
+const selectBarOptions = (country)=>{
+    country.forEach((c)=> countriesArr.push(c.name.common))
+    console.log(countriesArr);
+}
